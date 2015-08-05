87e7d5a0eea8ea26eeada982cfab9d4cbf03acb6
Jason Lane & Justin Scott <student@epicodus.com>
2015-08-05 10:48:41 -0700

    add first integration test

---
+++ spec/find_and_replace_integration_spec.rb
@@ -0,0 +1,15 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the find and replace path', {:type => :feature}) do
+  it('processes the user entry and replaces the first argument with the second argument') do
+    visit('/')
+    fill_in('fill_and_replace', :with => "I own a cat.")
+    fill_in('old_word', :with => 'cat')
+    fill_in('new_word', :with => 'dog')
+    click_button('Send')
+    expect(page).to have_content('I own a dog.')
+  end
+end
